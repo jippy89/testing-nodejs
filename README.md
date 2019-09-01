eb5f4322c7df6d24f537186a7bcc13941d04ee75
Jip Irfandy <irfandyjip98@gmail.com>
2019-09-01 21:54:48 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Thanks to the [article](https://medium.com/@asciidev/testing-a-node-express-application-with-mocha-chai-9592d41c0083)
---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import routes from './routes/index';// Instantiate express
+
+const app = express();// Set our port
+const port = process.env.PORT || 8000;// Configure app to user bodyParser
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());// Register our routes in app
+
+app.use('/', routes);// Start our server
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+module.exports = app
